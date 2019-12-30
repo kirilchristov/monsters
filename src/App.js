b88b99ae81dd567f986b3cd91acc9e05917372ea
kirilchristov <kiro.mm@gmail.com>
2019-12-29 17:59:58 -0800

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends Component {
+  construtor() {
+    super();
+
+    this.state = {
+      monsters: [];
+    }
+  }
+
+  compo
+}
+
+export default App;
