b3006542d3740123f8b36e068d2745d69327eb86
kirilchristov <kiro.mm@gmail.com>
2020-01-08 14:18:23 -0800

    Updates

---
+++ precache-manifest.9a7a9ec656302872b295a2ffb5ee8883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bef260e0d9704dcff804a24297ffeca",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "27b7362d965ea764c8c6",
+    "url": "/monsters/static/css/main.a2168f7d.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/monsters/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27b7362d965ea764c8c6",
+    "url": "/monsters/static/js/main.26dd0c78.chunk.js"
+  },
+  {
+    "revision": "f3f0459f8a3156a81a29",
+    "url": "/monsters/static/js/runtime-main.0e8a63f8.js"
+  }
+]);
